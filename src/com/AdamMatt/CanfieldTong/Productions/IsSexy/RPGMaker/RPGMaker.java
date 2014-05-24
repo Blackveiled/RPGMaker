4f42b14878e58e882e18f3649aa55ad65b35f72e
Blackveiled (Adam Canfield) <>
2014-05-24 05:41:33 +0200

    New:
    config.yml
    
    Modifications:
    
    You can now modify a few parameters for the plugin inside the config.yml file.

--- src/com/AdamMatt/CanfieldTong/Productions/IsSexy/RPGMaker/RPGMaker.java
+++ src/com/AdamMatt/CanfieldTong/Productions/IsSexy/RPGMaker/RPGMaker.java
@@ -18,6 +18,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class RPGMaker extends JavaPlugin {
 
     private static boolean debugMode = true;
+    private static String serverName;
+    private static String serverDescription;
+
+    // MySQL Database Information
+    private static String hostname;
+    private static String username;
+    private static String password;
+    private static String port;
+    private static String schema;
+    private static boolean mysqlEnabled = false;
 
     @Override
     public void onDisable() {
@@ -26,6 +36,16 @@ public class RPGMaker extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.getConfig();
+        serverName = this.getConfig().getString("RPGMaker.Server.Name");
+        serverDescription = this.getConfig().getString("RPGMaker.Server.Description");
+        hostname = this.getConfig().getString("RPGMaker.Database.Host");
+        username = this.getConfig().getString("RPGMaker.Database.User");
+        password = this.getConfig().getString("RPGMaker.Database.Password");
+        port = this.getConfig().getString("RPGMaker.Database.Port");
+        schema = this.getConfig().getString("RPGMaker.Database.Schema");
+        mysqlEnabled = this.getConfig().getBoolean("RPGMaker.Server.mysql-enabled");
+        debugMode = this.getConfig().getBoolean("RPGMaker.Server.debug-enabled");
         this.getServer().getPluginManager().registerEvents(new com.AdamMatt.CanfieldTong.Productions.IsSexy.RPGMaker.Social.SocialManager(), this);
         this.getServer().getPluginManager().registerEvents(new com.AdamMatt.CanfieldTong.Productions.IsSexy.RPGMaker.Events.RPGPlayerJoinServer(), this);
         this.getCommand("rpgmaker").setExecutor(new com.AdamMatt.CanfieldTong.Productions.IsSexy.RPGMaker.Commands.rpgmaker());
